df1cc088e9019ec9beb1b5cb3d50246fe6471af0
Brandon Coventry <bscoventry@gmail.com>
2023-02-03 19:04:40 -0600

    First Commit

---
+++ SPyke.py
@@ -0,0 +1,12 @@
+#--------------------------------------------------------------------------------------------------------------------------------------------
+# Authors: Brandon S Coventry            Wisconsin Institute for Translational Neuroengineering
+# Date: 02/03/2023                       The night it was so cold, my car battery died at work... Wisconsin is cold
+# Purpose: This is the header file for SPyke, a Python toolbox for analyzing Neural Spiking Data.
+# Interface: At the moment, this uses TDT based reading and analysis, but can be extended to other systems.
+# Revision History: Will be tracked in Github.
+# Notes: N/A
+#--------------------------------------------------------------------------------------------------------------------------------------------
+# Imports go here
+import numpy as np
+import tdt              #For reading in tdt files
+import matplotlib.pyplot as plt
