3cdea00dc979ea3ef3631447a6ea0a4ae6b2c14e
Brandon Coventry <bscoventry@gmail.com>
2024-03-06 12:31:58 -0600

    Added Chaos and MI calcs

--- batchITChaos.py
+++ batchITChaos.py
@@ -21,6 +21,7 @@ ISIs = [5]
 NPulse = [5]
 AClass = [5]
 df = pd.DataFrame(columns=['DataID', 'Electrode', 'EnergyPerPulse','ISI','NPulses','MI','KChaos','LFPMean','LFPSDER'])
+eng = matlab.engine.start_matlab()          #Use the matlab backend for Info theory and Chaos calcs
 for ck, word in enumerate(dataPath):
     stores = None             #Load all stores
     streamStore = 'streams'
@@ -34,10 +35,41 @@ for ck, word in enumerate(dataPath):
     NPul = NPulse[ck]
     if AClass[ck] == 5:
         power = np.array((-1.4, 37.2, 46.15, 58.6, 88, 94, 123, 182.62, 259, 313.6, 386.1, 414))
-    SpikeClass = Spike_Processed(dataPath,NPul,PW,ISI,power,stores,streamStore,debug,stim,SpksOrLFPs=SpksOrLFPs)
+    
+    SpikeClass = Spike_Processed(word,NPul,PW,ISI,power,stores,streamStore,debug,stim,SpksOrLFPs=SpksOrLFPs)
 
     #Spikes = SpikeClass.Spikes
     LFPs = SpikeClass.LFP
 
     epocedLFPs = SpikeClass.epocTrials(LFPs)
     sortedLFPs = SpikeClass.sortByStimCondition(epocedLFPs)
+    for electrode in sortedLFPs.keys():
+        curDataElec = sortedLFPs[electrode]
+        Rs = np.zeros((12,21))
+        R = np.zeros((1,1526))
+        counter = 1
+        Klist = np.zeros((12,))
+        for energies in curDataElec.keys():
+            curLFP = curDataElec[energies]
+            [hist,bins] = np.histogram(curLFP,bins=21)       #21 bins from ‘doane’ estimator
+            Rs[counter,:] = matlab.double(hist.tolist())
+            R = np.append(R,curLFP,axis=0)
+            meanLFP = np.mean(curLFP,axis=0)
+            meanLFP = matlab.double(meanLFP.tolist())
+            K = eng.chaos(meanLFP,0.5,'bvr','schreiber',2,'aaft_cpp')
+            Klist[counter] = K
+            counter = counter + 1
+            pdb.set_trace()
+        R = np.delete(R,0,0)
+        [Rhist,bins] = np.histogram(R,bins=21)
+        Rhist = matlab.double(Rhist.tolist())
+        Iarray = np.zeros((12,))
+        for bc in range(12):
+            [I, Iqe,bias] = eng.MIdrQE(Rs[bc],R)
+            Iarray[bc] = I-bias
+        
+
+            
+
+    
+    del SpikeClass             #Just for memory
