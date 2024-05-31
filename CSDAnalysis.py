da60a2a3973c4cc0360dcd2ef3e244d738b0d63d
Brandon Coventry <bscoventry@gmail.com>
2024-05-31 11:42:48 -0500

    Added Max/Min CSD Analysis

--- CSDAnalysis.py
+++ CSDAnalysis.py
@@ -24,22 +24,84 @@ def make_plot(xx, yy, zz, title='CSD', cmap=cm.bwr):
     ticks = np.linspace(-1 * t_max, t_max, 3, endpoint=True)
     plt.colorbar(im, orientation='horizontal', format='%.2f', ticks=ticks)
     return ax
-df = pd.read_pickle('sampleCSD.pkl')
-#[numrows,numcols] = np.shape(df)
-numrows = 1
+def parsePW(data):
+        
+        str1 = data.find("PW")
+        str2 = data.find("PU")
+        totStr = data[str2:str1+1]
+        SplitSTR = totStr.split("_")
+        if len(SplitSTR) == 2:
+            val = SplitSTR[1]
+            val = float(val[0:val.find("P")])
+        elif len(SplitSTR) == 3:
+            val = SplitSTR[2]
+            if val == '1P':
+                val = 0.1
+            elif val == '2P':
+                val = 0.2
+            elif val == '3P':
+                val = 0.3
+            elif val == '4P':
+                val = 0.4
+            elif val == '5P':
+                val = 0.5
+            elif val == '6P':
+                val = 0.6
+            elif val == '7P':
+                val = 0.7
+            elif val == '8P':
+                val = 0.8
+            elif val == '9P':
+                val = 0.9
+            else:
+                print('Error in conversion')
+        else:
+            print(data + ' Error')
+        return val
+# df1 = pd.read_pickle('LFPCSD0.pkl')
+# df2 = pd.read_pickle('LFPCSD1.pkl')
+# df3 = pd.read_pickle('LFPCSD3.pkl')
+# df4 = df1.append(df2, ignore_index=True)
+# df = df4.append(df3, ignore_index=True)
+df = pd.read_pickle('testCSD.pkl')
+[numrows,numcols] = np.shape(df)
+#numrows = 1
 EPP = df['EnergyPerPulse']
+uniqueEnergy = np.unique(EPP)
 ISI = df['ISI']
 xarray = df['xarray']
 yarray = df['yarray']
-pdb.set_trace()
+maxSink = {}
+maxSource = {}
+for jk in range(len(uniqueEnergy)):
+    maxSink[str(uniqueEnergy[jk])] = []
+    maxSource[str(uniqueEnergy[jk])] = []
 for ck in range(numrows):
-    curCSD = df['estCSD']#[ck]
-    maxCurrentSink = np.empty((1526,))
-    maxCurrentSource = np.empty((1526,))
+    PW = parsePW(df.DataID[ck])
+    ISI = df.ISI[ck]
+    nP = df.NPulses[ck]
+    stimTime = (nP*PW)+((nP-1)*ISI)
+    #Stim window + 100 ms
+    stimSamples = int(np.round(stimTime*(1526/1000))+153)
+    curCSD = df['estCSD'][ck]
+    maxCurrentSinkVec = np.empty((1526,))
+    maxCurrentSourceVec = np.empty((1526,))
     for bc in range(1526):
         curcurCSD = curCSD[:,:,bc]
-        maxCurrentSink[bc] = np.min(curcurCSD)
-        maxCurrentSource[bc] = np.max(curcurCSD)
-    maxSinkWhere = np.where(curCSD==maxCurrentSink)
-    maxSourceWhere = np.where(curCSD==maxCurrentSource)
+        maxCurrentSinkVec[bc] = np.min(curcurCSD)
+        maxCurrentSourceVec[bc] = np.max(curcurCSD)
+    maxCurrentSink = np.min(maxCurrentSinkVec[305:305+stimSamples])
+    maxCurrentSource = np.max(maxCurrentSourceVec[305:305+stimSamples])
+    curEPP = str(EPP[ck])
+    maxSink[curEPP].append(maxCurrentSink)
+    maxSource[curEPP].append(maxCurrentSource)
+    pdb.set_trace()
+    [xSink,ySink,tSink] = np.where(curCSD==maxCurrentSink)
+    [xSource,ySource,tSource] = np.where(curCSD==maxCurrentSource)
+    xSink = xSink[0]
+    ySink = ySink[0]
+    tSink = tSink[0]
+    xSource = xSource[0]
+    ySource = ySource[0]
+    tSource = tSource[0]
     
